e4150d9102f9f4dd45740891c50255ddbbc202d8
Simon J Stuart <lakraven@gmail.com>
2026-08-26 18:12:14 +0200

    [#16] Remove RTTI requirement from observer contract documentation

--- src/ESPressio_IObserver.hpp
+++ src/ESPressio_IObserver.hpp
@@ -4,9 +4,11 @@ namespace ESPressio {
 
     namespace Observable {
 
-        /// An `IObserver` is an object that can observe an `IObservable`
-        /// You MUST inherit from this type for ANY object from which you intend to Observe any `IObservable` descendant types
-        /// This is required to satisfy the `dynamic_cast` requirements of the `Observable` class, and cannot be avoided due to C++ language limitations.
+        /// An `IObserver` is an object that can observe an `IObservable`.
+        ///
+        /// Typed observer relationships are declared explicitly at registration
+        /// time. ESPressio Observable does not require RTTI, `typeid`, or
+        /// `dynamic_cast` to discover observer interfaces at runtime.
         class IObserver {
             public:
                 virtual ~IObserver() = default;
