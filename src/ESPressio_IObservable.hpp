1fb86698a35874f2dd30031beac779c045ca469a
Simon J Stuart <lakraven@gmail.com>
2026-08-27 08:54:23 +0200

    #17 Externalise Observable lifetime control allocation

--- src/ESPressio_IObservable.hpp
+++ src/ESPressio_IObservable.hpp
@@ -6,6 +6,7 @@
 #include <mutex>
 #include <stdexcept>
 
+#include <ESPressio_Memory.hpp>
 #include "ESPressio_IObserver.hpp"
 
 namespace ESPressio {
@@ -117,8 +118,6 @@ namespace ESPressio {
             };
         }
 
-        /// An `IObserverHandle` is returned when registering your Observer with an `IObservable`
-        /// It is used to not only check if the `IObservable` still exists, but also to unregister the Observer when desired!
         class IObserverHandle {
             public:
                 IObserverHandle() = default;
@@ -127,19 +126,13 @@ namespace ESPressio {
                 IObserverHandle(IObserverHandle&&) = delete;
                 IObserverHandle& operator=(IObserverHandle&&) = delete;
                 virtual ~IObserverHandle() = default;
-                /// Will Unregister this Observer from the `IObservable` if it still exists
                 virtual void Unregister() = 0;
-                /// Returns the associated `IObservable`, or nullptr once it has begun destruction.
-                /// The returned pointer is non-owning and must not be retained.
                 virtual IObservable* GetObservable() = 0;
-                /// Returns the non-owning `IObserver` pointer while registered,
-                /// or nullptr after unregistration has begun.
                 virtual IObserver* GetObserver() = 0;
         };
 
         using ObserverHandlePtr = std::unique_ptr<IObserverHandle>;
-    
-        /// An `IObservable` is an object that can be observed by any number of `IObserver` descendant types
+
         class IObservable : public std::enable_shared_from_this<IObservable> {
             private:
                 friend class ObserverHandle;
@@ -150,9 +143,6 @@ namespace ESPressio {
                     return _lifetimeControl;
                 }
 
-                /// Derived notification entry points must retain the returned token
-                /// for their complete body. This safely defers destruction requested
-                /// by a callback until the outer notification method unwinds.
                 std::shared_ptr<IObservable> AcquireNotificationLifetime() {
                     try {
                         return shared_from_this();
@@ -161,10 +151,6 @@ namespace ESPressio {
                     }
                 }
 
-                /// Any derived type whose state is used by registration methods must
-                /// invoke this at the start of its most-derived destructor, before
-                /// destroying or locking that state. This includes types derived from
-                /// a concrete Observable implementation when they override those methods.
                 void BeginObservableDestruction() noexcept {
                     _lifetimeControl->InvalidateAndWait();
                 }
@@ -172,25 +158,21 @@ namespace ESPressio {
             public:
                 IObservable()
                     : _lifetimeControl(
-                        std::make_shared<Detail::ObservableLifetimeControl>(this)) {}
+                        System::Memory::MakeShared<
+                            Detail::ObservableLifetimeControl,
+                            System::Memory::MemoryPolicy::ExternalPreferred
+                        >(this)) {}
                 IObservable(const IObservable&) = delete;
                 IObservable& operator=(const IObservable&) = delete;
                 IObservable(IObservable&&) = delete;
                 IObservable& operator=(IObservable&&) = delete;
-                /// Observable instances participating in notification must be owned
-                /// exclusively through std::shared_ptr. Invoking raw delete on a
-                /// shared-owned Observable is an ownership violation and undefined.
                 virtual ~IObservable() {
                     BeginObservableDestruction();
                 }
-                /// Will Unregister the `IObserver` from this `IObservable`
                 virtual void UnregisterObserver(IObserver* observer) = 0;
-                /// Will return `true` if the `IObserver` is registered with this `IObservable`
                 virtual bool IsObserverRegistered(IObserver* observer) = 0;
         };
 
-        /// Common interface for Observable implementations whose Observer
-        /// interfaces can be discovered automatically at notification time.
         class IUntypedObservable : public IObservable {
             public:
                 virtual ~IUntypedObservable() = default;
